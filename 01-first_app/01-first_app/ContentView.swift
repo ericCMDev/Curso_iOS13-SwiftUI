c0ce3740675babe8f0104f481934e32397fd9758
Eric CM <ecabestanym@gmail.com>
2020-09-19 17:59:33 +0200

    modificacion texto

--- 01-first_app/01-first_app/ContentView.swift
+++ 01-first_app/01-first_app/ContentView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//Si usamos .fot(largetitle) por ejemplo, cuando el usuario cambie el tamaño de su texto el el dispositivo se redimensionará haciéndose más grande o más pequeño de manera dinámica. Si por otra parte queremos que sea texto con tamaño fijo, haremos .size(35)
-        Text("Hello, world!")
-            .font(.largeTitle)
+//Si usamos .font(largetitle) por ejemplo, cuando el usuario cambie el tamaño de su texto el el dispositivo se redimensionará haciéndose más grande o más pequeño de manera dinámica. Si por otra parte queremos que sea texto con tamaño fijo, haremos .size(35)
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pharetra, justo nec vestibulum mattis, ligula purus sagittis ex, et ultrices neque risus ultrices metus. Integer blandit lacus tristique vehicula tempus. Proin facilisis auctor sapien, a consequat ligula fringilla eget. Sed porttitor dolor eget est ultrices bibendum.")
+            .font(.body)
             .foregroundColor(.blue)
+            .multilineTextAlignment(.leading)
+            .lineLimit(3).truncationMode(.middle)
             .padding()
     }
 }
