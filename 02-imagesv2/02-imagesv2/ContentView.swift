c2ba67d2e0a42b4cffbfba31bd5e625f29179f6e
Eric CM <ecabestanym@gmail.com>
2020-09-20 12:26:05 +0200

    first upload

---
+++ 02-imagesv2/02-imagesv2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  02-imagesv2
+//
+//  Created by Eric Cabestany Mena on 20/09/2020.
+//  Copyright © 2020 Eric Cabestany Mena. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
