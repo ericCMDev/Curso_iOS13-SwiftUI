e0490d2f76d5f01cd862ba442c60e66425575ad4
Eric CM <ecabestanym@gmail.com>
2020-09-20 11:00:52 +0200

    first upload second app

---
+++ 02-images/02-images/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  02-images
+//
+//  Created by Eric Cabestany Mena on 20/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 02-images/02-images/_2_imagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_imagesApp.swift
+//  02-images
+//
+//  Created by Eric Cabestany Mena on 20/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct _2_imagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
