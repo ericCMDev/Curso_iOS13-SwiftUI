56130b1c0ef0f5b42d89c9febe28354b0fd2ccba
Eric CM <ecabestanym@gmail.com>
2022-02-14 19:52:39 +0100

    primeraApp

---
+++ testApp1/testApp1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testApp1
+//
+//  Created by Eric Cabestany Mena on 14/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola, Eric!")
+            .fontWeight(.bold)
+            .background(.black)
+            .font(.system(.largeTitle, design: .rounded))
+            .foregroundColor(.white)
+            .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testApp1/testApp1/testApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  testApp1App.swift
+//  testApp1
+//
+//  Created by Eric Cabestany Mena on 14/2/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
