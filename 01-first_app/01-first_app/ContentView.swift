22637436915b940836499a293a9daff4ed702b52
Eric CM <ecabestanym@gmail.com>
2022-02-14 18:25:06 +0100

    first commit

---
+++ 01-first_app/01-first_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01-first_app
+//
+//  Created by Eric Cabestany Mena on 14/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
