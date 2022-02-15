205f09b401099ff5a55e6418a1faf4ce09f272c3
Eric CM <ecabestanym@gmail.com>
2022-02-15 19:39:26 +0100

    testOneApp completada

--- testOne/testOne/ContentView.swift
+++ testOne/testOne/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et efficitur felis. Vestibulum aliquam felis eget enim commodo laoreet. Praesent quis consectetur eros. Praesent maximus vitae tellus vel consectetur. ")
             .fontWeight(.bold)
-            .font(.system(.largeTitle).monospaced())
-            .foregroundColor(.blue)
+            .font(.system(.title))
+            .foregroundColor(.black)
+            .multilineTextAlignment(.center)
+            .lineLimit(nil)
+            .truncationMode(.tail)
+            .lineSpacing(5)
             .padding()
+//            .rotationEffect(.degrees(10), anchor: UnitPoint(x:0, y: 0))
+            .rotation3DEffect(.degrees(50), axis: (x: 1, y:0, z: 0))
+            .shadow(color: .gray, radius: 3, x: 4, y: 5)
     }
 }
 
