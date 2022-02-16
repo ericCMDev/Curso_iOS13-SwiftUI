90eaaa7740d7f333b0f57511af5bc25cceba4161
Eric CM <ecabestanym@gmail.com>
2022-02-16 20:15:55 +0100

    testApp3Stacks

---
+++ testApp3Stacks/testApp3Stacks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  testApp3Stacks
+//
+//  Created by Eric Cabestany Mena on 16/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Elige tu itinerario de aprendizaje")
+            .font(.system(.largeTitle, design: .rounded))
+            .fontWeight(.black)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testApp3Stacks/testApp3Stacks/testApp3StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp3StacksApp.swift
+//  testApp3Stacks
+//
+//  Created by Eric Cabestany Mena on 16/2/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp3StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
