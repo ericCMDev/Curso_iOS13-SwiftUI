5988ed14b0761be8952473067002223077e12e03
Eric CM <ecabestanym@gmail.com>
2022-02-17 20:19:36 +0100

    testApp3StacksDia2

--- testApp3Stacks/testApp3Stacks/ContentView.swift
+++ testApp3Stacks/testApp3Stacks/ContentView.swift
@@ -11,42 +11,26 @@ struct ContentView: View {
     var body: some View {
         VStack{
             HeaderView()
+            
             HStack{
-                VStack{
-                    Text("Básico")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                    Text("9.99€")
-                        .font(.system(size: 31, weight: .heavy, design: .rounded))
-                        .foregroundColor(.white)
-                    Text("Un curso incluído")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                    
-                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                 .padding(30)
-                 .background(Color.green)
-                 .cornerRadius(10)
+                PricingView(title: "Básico", subtitle: "Un Curso", price: "9.99€", textColor: .white, backgroundColor: .green)
                 
-                VStack{
-                    Text("Carrera")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.black)
-                    Text("29.99€")
-                        .font(.system(size: 30, weight: .heavy, design: .rounded))
-                        .foregroundColor(.black)
-                    Text("Toda una carrera")
-                        .font(.headline)
-                        .foregroundColor(.black)
+                ZStack {
+                    PricingView(title: "Carrera", subtitle: "Toda una carrera", price: "29.99€", textColor: .black, backgroundColor: Color(red:230/255, green:230/255, blue:230/255))
                     
-                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                 .padding(30)
-                 .background(Color(red:230/255, green:230/255, blue:230/255))
-                 .cornerRadius(10)
+                    Text("El mejor para empezar")
+                        .font(.system(.caption, design: .rounded))
+                        .foregroundColor(.white)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .background(Color(red:240/255, green:180/255, blue:50/255))
+                        .cornerRadius(10)
+                        .offset(x:0,y:-75)
+                        
+                }
+                
+                }.padding(.horizontal)
                  
-            }.padding(.horizontal)
             
 
         }
@@ -70,5 +54,34 @@ struct HeaderView: View {
                 .fontWeight(.black)
             
         }
+    }
+}
+
+struct PricingView: View {
+    
+    var title: String
+    var subtitle: String
+    var price: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    
+    var body: some View {
+        VStack{
+            Text(title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(textColor)
+            Text(price)
+                .font(.system(size: 30, weight: .heavy, design: .rounded))
+                .foregroundColor(textColor)
+            Text(subtitle)
+                .font(.headline)
+                .foregroundColor(textColor)
+            
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+            .padding(30)
+            .background(backgroundColor)
+            .cornerRadius(10)
     }
 }
