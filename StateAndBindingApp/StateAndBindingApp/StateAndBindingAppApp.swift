ff0083ba1444105a3664061b17810ca7c9e36df3
Eric CM <ecabestanym@gmail.com>
2022-03-09 20:22:23 +0100

    StateAndBindingApp

---
+++ StateAndBindingApp/StateAndBindingApp/StateAndBindingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndBindingAppApp.swift
+//  StateAndBindingApp
+//
+//  Created by Eric Cabestany Mena on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct StateAndBindingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
