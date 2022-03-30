79c96d48e2b4e43f25ce968349144343c496680e
Eric CM <ecabestanym@gmail.com>
2022-03-30 20:40:42 +0200

    DrawAppFirstCommit

---
+++ DrawApp/DrawApp/DrawAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawAppApp.swift
+//  DrawApp
+//
+//  Created by Eric Cabestany Mena on 30/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
