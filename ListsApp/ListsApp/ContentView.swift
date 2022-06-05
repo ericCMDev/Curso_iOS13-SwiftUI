1dfb8aa7fa95bcc0ab2fa9e285d4939ad2b22ce3
Eric CM <ecabestanym@gmail.com>
2022-06-05 11:55:50 +0200

    varis

--- ListsApp/ListsApp/ContentView.swift
+++ ListsApp/ListsApp/ContentView.swift
@@ -9,30 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var courseNames = [
-        "Curso 1",
-        "Curso 2",
-        "Curso 3",
-        "Curso 4"
+    
+    
+    var courses = [
+        Course(name: "Curso 1", image: "maths"),
+        Course(name: "Curso 2", image: "unity"),
+        Course(name: "Curso 3", image: "unreal"),
+        Course(name: "Curso 4", image: "uoc-logo")
     ]
     
-    var courseImages = ["maths", "unity", "unreal", "uoc-logo"]
-    
     var body: some View {
-        
-        
-        List(courseNames.indices, id: \.self){ index  in
-            HStack{
-                
-                Image(self.courseImages[index])
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 60, height: 60)
-                    .clipped()
-                    .cornerRadius(30)
-                Text(self.courseNames[index])
-            }
-            
+        List(courses){ course  in
+            CourseRow(course: course)
         }
     }
 }
@@ -40,5 +28,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct Course: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+}
+
+struct CourseRow: View {
+    var course: Course
+    var body: some View {
+        HStack{
+            
+            Image(course.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 60, height: 60)
+                .clipped()
+                .cornerRadius(30)
+            Text(course.name)
+        }
     }
 }
